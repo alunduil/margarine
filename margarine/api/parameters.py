b17f2e509e442333c742418baf0ff8ace3661417
Alex Brandt <alunduil@alunduil.com>
2013-04-29 11:34:09 -0500

    Add host paramter to mapid.

---
+++ margarine/api/parameters.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2013 by Alex Brandt <alex.brandt@rackspace.com>
+#
+# pycore is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import os
+
+from margarine.parameters import CONFIGURATION_DIRECTORY
+
+COMMON_API_PARAMETERS = [
+        { # --host=HOST, -h=HOST; HOST ← "127.0.0.1:5000"
+            "options": [ "--host", "-h" ],
+            "help": \
+                    "The host to bind the API daemon to and the address " \
+                    "plus port that will be listened on.",
+            },
+        ]
+
