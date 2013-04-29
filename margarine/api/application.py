1d2ae80142615cc8df0c2dc24d927178087f53b2
Alex Brandt <alunduil@alunduil.com>
2013-04-29 09:57:26 -0500

    Add API module with initialized flask application.

---
+++ margarine/api/application.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2013 by Alex Brandt <alex.brandt@rackspace.com>
+#
+# pycore is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+from flask import Flask
+
+APPLICATION = Flask("margarine")
+
