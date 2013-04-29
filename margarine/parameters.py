8fa73ad4a8e484ef7122d3bf00fdf0cd1f221b6b
Alex Brandt <alunduil@alunduil.com>
2013-04-29 11:34:54 -0500

    Add CONFIGURATION_DIRECTORY for margarine.

---
+++ margarine/parameters.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2013 by Alex Brandt <alex.brandt@rackspace.com>
+#
+# pycore is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import os
+
+CONFIGURATION_DIRECTORY = os.path.join(os.path.sep, "etc", "margarine")
+
