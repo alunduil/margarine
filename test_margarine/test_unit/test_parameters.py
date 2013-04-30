8e1d90c96a6a3ff2d3258cd28673d22199d3bcdf
Alex Brandt <alunduil@alunduil.com>
2013-04-30 13:59:27 -0500

    Add override test bodies.
    While adding override tests extra option mocks were created and we need
    to extract the correct parameter in legacy tests.

--- test_margarine/test_unit/test_parameters.py
+++ test_margarine/test_unit/test_parameters.py
@@ -23,6 +23,20 @@ def get_mock_parameters():
                 "help": \
                         "Help for the example item.",
                 },
+            { # --default=DEFAULT, -d=DEFAULT; DEFAULT ← "default"
+                "options": [ "--default", "-d" ],
+                "default": "default",
+                },
+            { # --configuration=CONFIGURATION, -c=CONFIGURATION 
+                "options": [ "--configuration", "-c" ],
+                },
+            { # --argument=ARGUMENT, -a=ARGUMENT
+                "options": [ "--argument", "-a" ],
+                },
+            { # --all=ALL, -a=ALL; ALL ← "default"
+                "options": [ "--all", "-A" ],
+                "default": "default",
+                },
             ]
     return parameters
 
@@ -134,7 +148,7 @@ class ParametersRespectsDoubleAsteriskTest(unittest2.TestCase):
 
         self.addCleanup(self.temp.close)
 
-        self.parameters = Parameters("test", self.temp.name, get_mock_parameters())
+        self.parameters = Parameters("test", self.temp.name, [get_mock_parameters()[0]])
 
     def test_dict_expansion(self):
         def parameters_to_dict(**kwargs):
@@ -145,18 +159,40 @@ class ParametersRespectsDoubleAsteriskTest(unittest2.TestCase):
 class ParametersResolutionTest(unittest2.TestCase):
     def setUp(self):
         self.temp = tempfile.NamedTemporaryFile()
+
         self.name = "test"
+        self.temp.write("[{0}]\n".format(self.name))
+        self.temp.write("all           = configuration\n")
+        self.temp.write("configuration = configuration\n")
+        self.temp.write("example       = configuration\n")
+
+        self.temp.seek(0)
 
         self.addCleanup(self.temp.close)
 
+        self.orig_argv = sys.argv
+        sys.argv = [ "foo", "--argument", "argument", "--all", "argument" ]
+
         self.parameters = Parameters(self.name, self.temp.name, get_mock_parameters())
 
+    def tearDown(self):
+        sys.argv = self.orig_argv
+
     def test_command_line(self):
-        self.fail()
+        self.assertEqual(self.parameters["argument"], "argument")
+        self.assertEqual(self.parameters["all"], "argument")
 
     def test_configuration_file(self):
-        self.fail()
+        self.assertEqual(self.parameters["configuration"], "configuration")
+        self.assertNotEqual(self.parameters["configuration"], "configuration")
 
     def test_default(self):
-        self.fail()
+        self.assertEqual(self.parameters["default"], "default")
+        self.assertNotEqual(self.parameters["all"], "default")
+
+    def test_override_argument(self):
+        self.assertEqual(self.parameters["all"], "argument")
+
+    def test_override_configuration(self):
+        self.assertEqual(self.parameters["example"], "configuration")
 
