b0538833ac562d8c47b0136cd5a69542fb2a34c0
Alex Brandt <alunduil@alunduil.com>
2013-04-30 21:29:04 -0500

    Add stub bookmark API.

---
+++ margarine/api/bookmark.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2013 by Alex Brandt <alex.brandt@rackspace.com>
+#
+# pycore is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+from margarine.api import APPLICATION
+from margarine.api import information
+
+@APPLICATION.route('/{i.API_VERSION}/bookmarks/'.format(i = information))
+def create_article():
+    pass
+
+@APPLICATION.route('/{i.API_VERSION}/bookmarks/<uuid>'.format(i = information))
+def show_bookmark(uuid):
+    pass
+
+@APPLICATION.route('/{i.API_VERSION}/bookmarks/<uuid>/<property'.format(i = information))
+def show_bookmark_property(uuid, property):
+    pass
+
