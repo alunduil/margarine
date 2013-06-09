3e72628cfe585b803fc905911224876ef16493b1
Alex Brandt <alunduil@alunduil.com>
2013-06-08 22:37:02 -0500

    Update another preamble.

--- margarine/aggregates/__init__.py
+++ margarine/aggregates/__init__.py
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2013 by Alex Brandt <alex.brandt@rackspace.com>
 #
-# pycore is freely distributable under the terms of an MIT-style license.
+# margarine is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 import sys
