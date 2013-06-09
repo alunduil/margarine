7d69fb73dee74d197fe57e52a5bc8ad52b9eb5a6
Alex Brandt <alunduil@alunduil.com>
2013-06-08 22:38:40 -0500

    Update another preamble.

--- margarine/api/article.py
+++ margarine/api/article.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2013 by Alex Brandt <alex.brandt@rackspace.com>
 #
-# pycore is freely distributable under the terms of an MIT-style license.
+# margarine is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 """Provide the blueprint for an articles API for flask.
