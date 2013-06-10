5fccd12754a5c037a15e7fdc31652b617c0d309a
Alex Brandt <alunduil@alunduil.com>
2013-06-10 10:16:49 -0500

    Update parameters unit tests.

--- test_margarine/test_unit/test_parameters.py
+++ test_margarine/test_unit/test_parameters.py
@@ -6,113 +6,124 @@
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 import unittest2
-import tempfile
-import argparse
-import sys
 
 from margarine.parameters import Parameters
 
-def get_mock_parameters():
-    parameters = [
-            { # --example=FOO, -e=FOO; FOO ← "bar"
-                "options": [ "--example", "-e" ],
-                "default": "bar",
-                "help": \
-                        "Help for the example item.",
-                },
-            { # --default=DEFAULT, -d=DEFAULT; DEFAULT ← "default"
-                "options": [ "--default", "-d" ],
-                "default": "default",
-                },
-            { # --configuration=CONFIGURATION, -c=CONFIGURATION 
-                "options": [ "--configuration", "-c" ],
-                },
-            { # --argument=ARGUMENT, -a=ARGUMENT
-                "options": [ "--argument", "-a" ],
-                },
-            { # --all=ALL, -a=ALL; ALL ← "default"
-                "options": [ "--all", "-A" ],
-                "default": "default",
-                },
-            { # --configuration_only=CONFIGURATION, -C=CONFIGURATION
-                "options": [ "--configuration_only", "-C" ],
-                "only": "configuration",
-                },
-            { # --argument_only=ARGUMENT, -A=ARGUMENT
-                "options": [ "--argument_only", "-O" ],
-                "only": "arguments",
-                },
-            ]
-    return parameters
-
+TEST_PARAMETERS = [
+        { # --default=DEFAULT, -d=DEFAULT; DEFAULT ← "default"
+            "options": [ "--default", "-d" ],
+            "default": "default",
+            },
+        { # --environment=ENV, -e=ENV; ENV ← "environment"
+            "options": [ "--environment", "-e" ],
+            "default": "environment",
+            },
+        { # --configuration=CONF, -c=CONF; CONF ← "configuration"
+            "options": [ "--configuration", "-c" ],
+            "default": "configuration",
+            },
+        { # --argument=ARG, -a=ARG; ARG ← "argument"
+            "options": [ "--argument", "-a" ],
+            "default": "argument",
+            },
+        { # --environment_only=ENV, -E=ENV; ENV ← "environment"
+            "options": [ "--environment_only", "-E" ],
+            "default": "environment",
+            "only": "environment",
+            },
+        { # --configuration-only=CONF, -C=CONF; CONF ← "configuration"
+            "options": [ "--configuration-only", "-C" ],
+            "defaults": "configuration",
+            "only": "configuration",
+            },
+        { # --argument-only=ARG, -A=ARG; ARG ← "argument"
+            "options": [ "--argument-only", "-A" ],
+            "defaults": "argument",
+            "only": "argument",
+            },
+        ]
+            
 class ParametersConstructionTest(unittest2.TestCase):
-    def setUp(self):
-        self.parameters = get_mock_parameters()
-
-        self.orig_argv = sys.argv
-        sys.argv = [ "foo"]
-        
     def tearDown(self):
-        sys.argv = self.orig_argv
+        Parameters().__dict__ = {}
 
-    def test_construction(self):
-        temp = tempfile.NamedTemporaryFile()
+    def test_zero_parameters_construction(self):
+        parameters = Parameters()
 
-        parameters = Parameters("test", temp.name, self.parameters)
+        self.assertIsInstance(parameters, Parameters)
 
-        temp.close()
+    def test_one_parameter_construction(self):
+        parameters = Parameters("unused")
+
+        self.assertIsInstance(parameters, Parameters)
+
+    def test_passed_parameters_construction(self):
+        parameters = Parameters(parameters = TEST_PARAMETERS)
+
+        self.assertIsInstance(parameters, Paraemters)
+
+    def test_passed_file_construction(self):
+        fh = tempfile.NamedTemporaryFile()
+
+        parameters = Parameters(file_path = fh.name)
+
+        self.assertIsInstance(parameters, Parameters)
+
+    def test_borg_construction(self):
+        p1 = Parameters()
+        p2 = Parameters()
+
+        self.assertIs(p1.__dict__, p2.__dict__)
 
 class ParametersRespectsDoubleAsteriskTest(unittest2.TestCase):
     def setUp(self):
-        self.temp = tempfile.NamedTemporaryFile()
+        self.parameters = Parameters()
 
-        self.addCleanup(self.temp.close)
-
-        self.parameters = Parameters("test", self.temp.name, [get_mock_parameters()[0]])
+    def tearDown():
+        Parameters().__dict__ = {}
 
     def test_dict_expansion(self):
         def parameters_to_dict(**kwargs):
             return kwargs
 
-        self.assertEqual(parameters_to_dict(**self.parameters), { "example": "bar" })
+        self.assertEqual(parameters_to_dict(**self.parameters), {})
 
 class ParametersResolutionTest(unittest2.TestCase):
     def setUp(self):
+        self.name = "test_section"
+
+        # TODO Mock the environment.
+
         self.temp = tempfile.NamedTemporaryFile()
 
-        self.name = "test"
         self.temp.write("[{0}]\n".format(self.name))
-        self.temp.write("all           = configuration\n")
         self.temp.write("configuration = configuration\n")
-        self.temp.write("example       = configuration\n")
+        self.temp.write("argument      = configuration\n")
 
         self.temp.seek(0)
 
         self.addCleanup(self.temp.close)
 
         self.orig_argv = sys.argv
-        sys.argv = [ "foo", "--argument", "argument", "--all", "argument" ]
+        sys.argv = [ "test_script", "--argument=argument" ]
 
-        self.parameters = Parameters(self.name, self.temp.name, get_mock_parameters())
+        self.parameters = Parameters(self.name, self.temp.name, TEST_PARAMETERS)
+        self.parameters.parse()
 
     def tearDown(self):
         sys.argv = self.orig_argv
 
-    def test_command_line(self):
-        self.assertEqual(self.parameters["argument"], "argument")
-        self.assertEqual(self.parameters["all"], "argument")
-
-    def test_configuration_file(self):
-        self.assertEqual(self.parameters["configuration"], "configuration")
-        self.assertNotEqual(self.parameters["all"], "configuration")
+        self.parameters.__dict__ = {}
 
     def test_default(self):
-        self.assertEqual(self.parameters["default"], "default")
-        self.assertNotEqual(self.parameters["all"], "default")
+        self.assertEqual(self.parameters["{0}.default".format(self.name)], "default")
 
-    def test_override_argument(self):
-        self.assertEqual(self.parameters["all"], "argument")
+    def test_environment(self):
+        self.assertEqual(self.parameters["{0}.environment".format(self.name)], "environment")
 
-    def test_override_configuration(self):
-        self.assertEqual(self.parameters["example"], "configuration")
+    def test_configuration(self):
+        self.assertEqual(self.parameters["{0}.configuration".format(self.name)], "configuration")
+
+    def test_argument(self):
+        self.assertEqual(self.parameters["{0}.argument".format(self.name)], "argument")
 
