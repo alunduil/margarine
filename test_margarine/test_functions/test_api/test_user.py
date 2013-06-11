5892d6a3480d4472292954c1a7414f8aaf38789b
Alex Brandt <alunduil@alunduil.com>
2013-06-10 19:55:58 -0500

    Add test for user creation.

---
+++ test_margarine/test_functions/test_api/test_user.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2013 by Alex Brandt <alex.brandt@rackspace.com>
+#
+# margarine is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import unittest2
+import logging
+
+from margarine.api import information
+from margarine.api import MARGARINE_API
+
+logger = logging.getLogger(__name__)
+
+class UserCreationTest(unittest2.TestCase):
+    def setUp(self):
+        MARGARINE_API.config["TESTING"] = True
+        self.application = MARGARINE_API.test_client()
+        self.account_name = "test_user"
+
+    def test_signup_request(self):
+        url = "/{i.API_VERSION}/users/{username}"
+        url.format(username = self.account_name, i = information)
+        
+        response = self.application.put(url, data = {
+            "email": "test@example.com",
+            })
+
+        logger.debug("response: %s", response)
+        logger.debug("response.status: %s", response.status)
+        logger.debug("response.__dict__: %s", response.__dict__.keys())
+
+        self.assertIn("202", response.status)
+
