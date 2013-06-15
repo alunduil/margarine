b06dfb005865e7197a3d4fc01060bd72d8b77362
Alex Brandt <alunduil@alunduil.com>
2013-06-15 14:50:59 -0500

    Add READ and DELETE tests for user API.

--- test_margarine/test_unit/test_api/test_user.py
+++ test_margarine/test_unit/test_api/test_user.py
@@ -16,7 +16,11 @@ class UserCreationTest(unittest2.TestCase):
     def setUp(self):
         MARGARINE_API.config["TESTING"] = True
         self.application = MARGARINE_API.test_client()
+
         self.account_name = "test_user"
+
+        self.url = "/{i.API_VERSION}/users/{username}"
+        self.url = self.url.format(username = self.account_name, i = information)
 
         patcher = mock.patch("margarine.api.user.get_channel")
         mock_get_channel = patcher.start()
@@ -28,10 +32,9 @@ class UserCreationTest(unittest2.TestCase):
     def test_user_creation_request(self):
         """Create a new (non-existent) user."""
 
-        url = "/{i.API_VERSION}/users/{username}"
-        url = url.format(username = self.account_name, i = information)
+        # TODO Mock user retrieval (non-existent user).
 
-        response = self.application.put(url, data = {
+        response = self.application.put(self.url, data = {
             "email": "test@example.com",
             })
 
@@ -39,19 +42,50 @@ class UserCreationTest(unittest2.TestCase):
 
         # Ensure publish has a routing_key parameter of users.create.
 
+    def test_existing_user_read_request(self):
+        """Read an existing user."""
+
+        # TODO Mock user retrieval (existing user).
+
+        response = self.application.get(self.url)
+
+        self.assertIn("200", response.status)
+
+    def test_nonexistent_user_read_request(self):
+        """Read a non-existent user."""
+
+        # TODO Mock user retrieval (non-existent user).
+
+        resposne = self.application.get(self.url)
+
+        self.assertIn("404", response.status)
+
     def test_user_update_request(self):
         """Update an existing user."""
 
-        # TODO Ensure user exists.
+        # TODO Mock user retrieval.
 
-        url = "/{i.API_VERSION}/users/{username}"
-        url = url.format(username = self.account_name, i = information)
-
-        response = self.application.put(url, data = {
+        response = self.application.put(self.url, data = {
             "email": "test@example.com",
+            "name": "Test User",
             })
 
         self.assertIn("202", response.status)
 
         # Ensure publish has a routing_key parameter of users.update.
+
+    def test_user_delete_request(self):
+        """Delete an existing user."""
+
+        # TODO Mock user retrieval.
+
+        response = self.application.delete(self.url)
+
+        self.assertIn("200", response.status)
+
+        # And again to prove idempotency:
+
+        response = self.application.delete(self.url)
+
+        self.assertIn("200", response.status)
 
