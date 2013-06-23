a7bbfe3edbb63422593e9b91c983cb14e3aa99bc
Alex Brandt <alunduil@alunduil.com>
2013-06-23 16:01:08 -0500

    Moving aggregates to a module rather than a package.
    The aggregates functionality that was planned is over-engineered and not
    necessary with the capabilities that the pymongo driver provides.

---
+++ margarine/aggregates.py
@@ -0,0 +1,53 @@
+# -*- coding: UTF-8 -*-
+#
+# Copyright (C) 2013 by Alex Brandt <alex.brandt@rackspace.com>
+#
+# margarine is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import pymongo
+
+from margarine.parameters import Parameters
+
+Parameters("datastore", parameters = [
+    { # --datastore-url=URL; URL ← mongo://localhost:27017/test
+        "options": [ "--url" ],
+        "default": "mongo://localhost:27017/test",
+        "help": \
+                "The URL endpoint of the data store mechanism.  This can be " \
+                "a local sqlite database but typically will be set to a " \
+                "MongoDB instance.",
+        }
+    ])
+
+DATASTORE_CONNECTION = None
+
+def get_collection(collection):
+    """Using the datastore.url parameter we get a collection for storing data.
+
+    If a connection has already been established we'll re-use that connection;
+    otherwise, we'll create the connection and return the requested collection.
+
+    .. note::
+        Currently we only work with a Mongo datastore using the pymongo binding
+        layer.  If we decide to use other datastores we'll have to re-evaluate
+        this architecture.
+
+    Returns
+    -------
+
+    A collection for datastore interaction.
+
+    """
+
+    global DATASTORE_CONNECTION
+
+    uri = URI(Parameters()["datastore.url"])
+
+    if DATASTORE_CONNECTION is None:
+        DATASTORE_CONNECTION = MongoClient(uri.host)
+
+    database = DATASTORE_CONNECTION[uri.path]
+
+    return database[collection]
+
