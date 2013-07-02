63de3b42be97092cfb5e7ca08218f71fa63b3025
Alex Brandt <alunduil@alunduil.com>
2013-07-02 09:04:13 -0500

    Add email wrapper.

--- margarine/communication.py
+++ margarine/communication.py
@@ -5,6 +5,12 @@
 # margarine is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+"""All communication mechanisms.
+
+This module includes queue channel acquisition and email handlers.
+
+"""
+
 import pika
 import logging
 
@@ -13,8 +19,8 @@ from margarine.helpers import URI
 
 logger = logging.getLogger(__name__)
 
-Parameters("communication", parameters = [
-    { # --communication-url=URL; URL ← local
+Parameters("queue", parameters = [
+    { # --queue-url=URL; URL ← amqp://guest:guest@localhost
         "options": [ "--url" ],
         "default": "amqp://guest:guest@localhost",
         "help": \
@@ -65,3 +71,61 @@ def get_channel():
 
     return CONNECTION_BROKER.channel()
 
+Parameters("email", parameters = [
+    { # --email-url=URL; URL ← smtp://localhost
+        "options": [ "--url" ],
+        "default": "smtp://localhost",
+        "help": \
+                "The URL endpoint of the email relay server used by the " \
+                "node.",
+        },
+    { # --email-from=EMAIL; EMAIL ← noreply@HOSTNAME
+        "options": [ "from" ],
+        "default": "noreply@" + socket.gethostname(),
+        "help": \
+                "The email address used as the FROM address on all " \
+                "generated emails.",
+        },
+    ])
+
+def send_user_email(user, verification):
+    """Send an email for user sign up or another action.
+
+    This method encapsulates email handling of the application.  It proxies to
+    other methods that correspond to specific actions required for the message
+    being handled.
+
+    Parameters
+    ----------
+
+    :user:         The user's information to personalize the email.
+    :verification: The verification token for verifying a new user.
+
+    This method actually sends the email that is generated and does not return
+    anything.
+
+    """
+
+    # TODO i18n this stuff!
+    message_text = \
+            "Thank you for registering for Margarine.  We hope you enjoy " \
+            "the services provided.\n" \
+            "\n" \
+            "Please, verify your human status by visiting the following " \
+            "URL: {verification_url}\n" \
+            "\n" \
+            "\n" \
+            "Thanks,\n" \
+            "\n",
+            "Margarine\n"
+
+    message = MIMEText(message_text.format(verification_url = url_for("user.verification", username = user["username"], verification = verification)))
+
+    message["Subject"] = "Margarine Verification"
+    message["From"] = "Margarine Verifications <" + Parameters()["email.from"] + ">"
+    message["To"] = "{user[name]} <{user[email]}>".format(user = user)
+    
+    _ = smtplib.SMTP(Parameters()["email.url"])
+    _.sendmail(Parameters()["email.from"], [ user["email"] ], message.as_string())
+    _.quit()
+    
