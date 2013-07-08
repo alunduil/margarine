c9167f1c067841902a5ef381db4bd5d33d3c6c3d
Alex Brandt <alunduil@alunduil.com>
2013-07-08 15:56:04 -0500

    Add stubs for reference and sanitization consumers.

--- margarine/consumers/articles.py
+++ margarine/consumers/articles.py
@@ -29,22 +29,115 @@ def create_article_consumer(channel, method, header, body):
     * Submits a reference job to update automatic notations in this and others.
     * Submits the sanitization request for the HTML body. 
 
+    .. note::
+        The article will need a pre-allocated space in MongoDB for performance
+        reasons.  The way we start with the bare necesities and then add
+        information at a later time means we may get fragmentation in the data
+        stored if we don't pre-allocate enough space.
+
     """
 
     article = json.loads(body)
 
     logger.debug("article: %s", article)
 
+    _id = article["_id"]
+
     articles = get_collection("articles")
 
     _ = articles.find_one({ "_id": article["_id"] })
 
-    if _ is None: # Article doesn't already have meta-information.
+    logger.debug("Found: %s", _)
+
+    if _ is None or "created_at" not in _:
         article["created_at"] = datetime.datetime.now()
 
-     
-    
+    article = dict([ (k, v) for k, v in article.iteritems() if k not in _ or v != _[k] ])
+
+    logger.debug("article: %s", article)
+
+    get_collection("articles").update({ "_id": article["_id"] }, { "$set": article }, upsert = True)
+
+    message_properties = pika.BasicProperties()
+    message_properties.content_type = "application/json"
+    message_properties.durable = False
+
+    message = json.dumps({ "_id": article["_id"] })
+
+    _ = get_channel()
+    _.exchange_declare(exchange = "margarine.articles.create", type = "fanout", auto_delete = False)
+    _.basic_publish(body = message, exchange = "margarine.articles.create", properties = message_properties, routing_key = "articles.create")
+
     channel.basic_ack(delivery_tag = method.delivery_tag)
+
+def update_reference_consumer(channel, method, header, body):
+    """Update the references to and from the article specified.
+
+    This takes the UUID of the article being added and searches it for links to
+    articles (updating the remote references) and then kicks off a temporary
+    map reduce process to find other articles that point back at this article
+    completing the citation circle.
+
+    This updates two types of notations:
+
+    * citations
+    * references
+
+    These are updated on the article in question and the remote article for
+    every link found.  This is an extremely heavy process once we have more
+    than a trivial amount of articles in the data store.
+
+    .. note::
+        This is an easy area for improvement with algorithms on search.
+
+    """
+
+    article = json.loads(body)
+
+    logger.debug("article: %s", article)
+
+    article = get_collection("articles").find_one({ "_id": article["_id"] })
+
+    # TODO Implement the following general algorithm:
+    # Retrieve the raw HTML
+    # Find all links to other cataloged articles
+    # Update those articles with a citation reference to this article
+    # Update this article with a reference reference to the remote articles
+    # Search all bodies of all articles for links to this article
+    # Update those articles with a reference reference to this article
+    # Update this article with a citation reference to the remote articles.
+
+def sanitize_html_consumer(channel, method, header, body):
+    """Download and sanitize the HTML for the given article.
+
+    The HTML should be simplified as much as possible without modifying the
+    feel of the structure to someone reading the content of the body of the
+    document.
+
+    .. note::
+        Analysis will be necessary that shows the statistics on sanitized HTML
+        size for a determination as to whether we can store it inline in Mongo
+        or out of band in an object store like Rackspace Cloud Files.
+
+    The decisions and algorithms used for streamlining the HTML are not
+    proprietary in any way and can be used and modified under the terms of this
+    file's licensing but more importantly can be improved or modified if 
+    imperfections are found.
+
+    """
+
+    article = json.loads(body)
+
+    logger.debug("article: %s", article)
+
+    article = get_collection("articles").find_one({ "_id": article["_id"] })
+
+    # TODO Implement the following general algorithm:
+    # Obtain the raw HTML for the site.
+    # Record the obtained meta-information
+    # Sanitize the HTML
+    # Log the size of the sanitized HTML
+    # Store the sanitized HTML somewhere and record a reference to this
 
 def register(channel):
     """Register the article worker functions on the passed channel.
@@ -58,11 +151,22 @@ def register(channel):
     
     """
 
-    # TODO Use a fanout exchange for article creation?
-    channel.exchange_declare(exchange = "margarine.articles.topic", type = "topic", auto_delete = True)
+    channel.exchange_declare(exchange = "margarine.articles.topic", type = "topic", auto_delete = False)
 
-    channel.queue_declare(queue = "margarine.articles.create", auto_delete = True)
+    channel.queue_declare(queue = "margarine.articles.create", auto_delete = False)
     channel.queue_bind(queue = "margarine.articles.create", exchange = "margarine.articles.topic", routing_key = "articles.create")
 
-    channel.basic_consume(create_user_consumer, queue = "margarine.articles.create", no_ack = False, consumer_tag = "create")
+    channel.basic_consume(create_article_consumer, queue = "margarine.articles.create", no_ack = False, consumer_tag = "create")
+
+    channel.exchange_declare(exchange = "margarine.articles.create", type = "fanout", auto_delete = False)
+
+    channel.queue_declare(queue = "margarine.articles.references", auto_delete = False)
+    channel.queue_bind(queue = "margarine.articles.references", exchange = "margarine.articles.create", routing_key = "articles.create")
+
+    channel.basic_consume(update_references_consumer, queue = "margarine.articles.references", no_ack = True, consumer_tag = "references")
+
+    channel.queue_declare(queue = "margarine.articles.sanitization", auto_delete = False)
+    channel.queue_bind(queue = "margarine.articles.sanitization", exchange = "margarine.articles.create", routing_key = "articles.create")
+
+    channel.basic_consume(sanitize_html_consumer, queue = "margarine.articles.sanitization", no_ack = True, consume_tag = "references")
 
