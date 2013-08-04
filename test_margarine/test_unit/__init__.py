b12818bdfa244dc67e138393efe7e433992091e9
Alex Brandt <alunduil@alunduil.com>
2013-08-04 15:15:14 -0500

    Add base margarine test case (now with mocking!).

--- test_margarine/test_unit/__init__.py
+++ test_margarine/test_unit/__init__.py
@@ -3,3 +3,90 @@
 # margarine is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+import unittest
+import mock
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+class BaseMargarineTest(unittest.TestCase):
+    def setUp(self):
+        super(BaseMargarineTest, self).setUp()
+
+        self._set_up_mocks(getattr(self, 'mock_mask', ()))
+
+    def _get_patch_mock(self, mock_function):
+        '''Patch and return a mock for the specified function.
+
+        Correlates to the currently running test to streamline patching local
+        and mocking items.
+
+        Arguments
+        ---------
+
+        :``mock_function``: The function name to mock in the local module scope
+
+        '''
+
+        logger.debug('patching: __module__.__name__: %s', __module__.__name__)
+        logger.debug('patching: __class__.__name__: %s', __class__.__name__)
+        logger.debug('patching: __name__: %s', __name__)
+
+        patcher = mock.patch([
+            __module__.__name__.replace('test_', '').replace('unit', ''),
+            __class__.__name__.replace('test_', ''),
+            mock_function,
+            ].join('.'))
+
+        self.addCleanup(patcher.stop)
+
+        return patcher.start()
+
+    def _get_attached_mock(self, mock_function):
+        '''Attach a mock to the return value of the mock_function.
+
+        It's common (in this application anyway) to need to mock the return
+        value of a function rather than just the function itself.  This
+        function factors that idiom out into one location.
+
+        Arguments
+        ---------
+
+        :``mock_function``: The mock that we are overriding the return value of
+
+        '''
+
+        _ = mock_function
+        desired_mock = mock.MagicMock()
+        _.return_value = desired_mock
+
+        return desired_mock
+
+    def _set_up_mocks(self, mock_mask = ()):
+        '''Set up all mocks except those specified in the mock_mask.
+
+        Sets up all mocks required by margarine except those mocks that are
+        specified in the mock mask (read from self.mock_mask in setUp if
+        present).
+
+        Arguments
+        ---------
+
+        :``mock_mask``: The list of mocks to not setup.  Used for controlling
+                        what portions are not mocked during integration and
+                        functional testing.
+
+        '''
+
+        if 'channel' not in mock_mask:
+            self.mock_channel = _get_attached_mock(_get_patch_mock('get_channel'))
+
+        # TODO Update to datastores
+        if 'collection' not in mock_mask:
+            self.mock_collection_ = _get_attached_mock(_get_patch_mock('get_collection'))
+
+        # TODO Update to datastores
+        if 'keyspace' not in mock_mask:
+            self.mock_keyspace = _get_attached_mock(_get_patch_mock('get_keyspace'))
+
