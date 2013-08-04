a8614ddc9d2fa69195ccb903560efbf0defdd09b
Alex Brandt <alunduil@alunduil.com>
2013-08-04 14:30:24 -0500

    Add base blend test case.

--- test_margarine/test_unit/test_blend/__init__.py
+++ test_margarine/test_unit/test_blend/__init__.py
@@ -3,3 +3,22 @@
 # margarine is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+import unittest
+import mock
+
+import logging
+
+from test_margarine.test_unit import BaseMargarineTest
+
+from margarine.blend import information
+from margarine.blend import BLEND
+
+logger = logging.getLogger(__name__)
+
+class BaseBlendTest(BaseMargarineTest):
+    def setUp(self):
+        super(BaseBlendTest, self).setUp()
+
+        BLEND.config['TESTING'] = True
+        self.application = BLEND.test_client()
+
