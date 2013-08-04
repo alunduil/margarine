11a7bfba3cccbeb7f1727235cb23026e971cbf0a
Alex Brandt <alunduil@alunduil.com>
2013-08-04 12:13:39 -0500

    Run lettuce first and ensure proper rundir.

--- test_margarine/__main__.py
+++ test_margarine/__main__.py
@@ -4,7 +4,12 @@
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 import nose
+import lettuce.bin
+import os
 
 if __name__ == "__main__":
+    os.chdir(os.path.dirname(__file__))
+    lettuce.bin.main()
+
     nose.main()
 
