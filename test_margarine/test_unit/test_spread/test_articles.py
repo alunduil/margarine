1d4763999dd824b3adc1b52382555e7cdfd79380
Alex Brandt <alunduil@alunduil.com>
2013-08-07 16:47:07 -0500

    Add stubs for spread tests.

--- test_margarine/test_unit/test_spread/test_articles.py
+++ test_margarine/test_unit/test_spread/test_articles.py
@@ -1,14 +1,18 @@
+# -*- coding: UTF-8 -*-
+#
 # Copyright (C) 2013 by Alex Brandt <alex.brandt@rackspace.com>
 #
 # margarine is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+import mock
+
 import logging
 
-from test_margarine.test_unit import BaseMargarineTest
+from test_margarine.test_unit.test_spread import BaseSpreadTest
 
 logger = logging.getLogger(__name__)
 
-class BaseSpreadTest(BaseMargarineTest):
+class BaseSpreadArticleTest(BaseSpreadTest):
     pass
 
