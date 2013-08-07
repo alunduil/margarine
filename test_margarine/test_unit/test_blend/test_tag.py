22b83c87d5c5ea0e2a66837d24973773bdf380b9
Alex Brandt <alunduil@alunduil.com>
2013-08-07 17:16:25 -0500

    Add tag test stub to blend.

---
+++ test_margarine/test_unit/test_blend/test_tag.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+#
+# Copyright (C) 2013 by Alex Brandt <alex.brandt@rackspace.com>
+#
+# margarine is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import logging
+
+from test_margarine.test_unit.test_blend import BaseBlendTest
+
+from margarine.blend import information
+
+logger = logging.getLogger(__name__)
+
+class BaseBlendTagTest(BaseBlendTest):
+    def setUp(self):
+        super(BaseBlendArticleTest, self).setUp()
+
+        self.tags = []
+
+        self.base_url = '/{i.API_VERSION}/tags/'.format(i = information)
