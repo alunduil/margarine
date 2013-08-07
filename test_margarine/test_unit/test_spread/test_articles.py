278692899cecc7376c235b5c28035b78daa63c4a
Alex Brandt <alunduil@alunduil.com>
2013-08-07 18:26:06 -0500

    Add spread article create test.

--- test_margarine/test_unit/test_spread/test_articles.py
+++ test_margarine/test_unit/test_spread/test_articles.py
@@ -8,11 +8,53 @@
 import mock
 
 import logging
+import json
 
 from test_margarine.test_unit.test_spread import BaseSpreadTest
+
+# TODO Remove pluralization of articles
+from margarine.spread.articles import create_article_consumer
 
 logger = logging.getLogger(__name__)
 
 class BaseSpreadArticleTest(BaseSpreadTest):
-    pass
+    def setUp(self):
+        super(BaseSpreadArticleTest, self).setUp()
 
+        # TODO Merge with articles from test_blend.test_article?
+        self.articles = [
+                'http://blog.alunduil.com/posts/an-explanation-of-lvm-snapshots.html',
+                'http://developer.rackspace.com/blog/got-python-questions.html',
+                ]
+
+        self.articles = [ { '_id': uuid.uuid5(uuid.NAMESPACE_URL, _), 'url': _ } for _ in self.articles ]
+
+class SpreadArticleCreate(BaseSpreadArticleTest):
+    def test_article_create_unsubmitted(self):
+        '''Spread::Article Create—Unsubmitted
+
+        .. note::
+            Tests first submission of the article (nothing stored yet).
+
+        '''
+
+        self.mock_channel.find_one.return_value = None
+
+        method = mock.MagicMock()
+        method.delivery_tag.return_value = 'create'
+
+        for article in self.articles:
+            create_article_consumer(mock.MagicMock(), method, None, json.dumps(article))
+
+            _id = article.pop('_id')
+
+            self.mock_channel.find_one.assert_called_once_with({ '_id': _id })
+            self.mock_channel.update.assert_called_once_with({ '_id': _id }, { '$set': article }, upsert = True)
+            self.mock_channel.reset_mock()
+
+            self.mock_channel.basic_publish.assert_called_once_with(
+                    body = json.dumps({ '_id': _id }),
+                    exchange = 'margarine.articles.create',
+                    properties = mock.ANY,
+                    routing_key = 'articles.create')
+            self.mock_channel.reset_mock()
