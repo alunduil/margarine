9d93b69d9bd57f1072b0f3eafe4c4e0a6ca3dbbc
Alex Brandt <alunduil@alunduil.com>
2013-08-07 20:29:35 -0500

    Add datetime mock to Article Create Consumer.

--- test_margarine/test_unit/test_spread/test_articles.py
+++ test_margarine/test_unit/test_spread/test_articles.py
@@ -10,6 +10,7 @@ import mock
 import logging
 import uuid
 import json
+import datetime
 
 from test_margarine.test_unit.test_spread import BaseSpreadTest
 
@@ -41,15 +42,25 @@ class SpreadArticleCreate(BaseSpreadArticleTest):
 
         '''
 
+        test_datetime = datetime.datetime(2013, 8, 7, 20, 25, 41, 596627)
+
         method = mock.MagicMock()
         method.delivery_tag.return_value = 'create'
 
         for article in self.articles:
             self.mock_collection.find_one.return_value = None
 
-            create_article_consumer(mock.MagicMock(), method, None, json.dumps(article))
+            with mock.patch('.'.join([
+                self.__module__.replace('test_', '').replace('unit.', ''),
+                'datetime',
+                ])) as mock_datetime:
+
+                mock_datetime.datetime.now.return_value = test_datetime
+
+                create_article_consumer(mock.MagicMock(), method, None, json.dumps(article))
 
             _id = article.pop('_id')
+            article['created_at'] = test_datetime
 
             self.mock_collection.find_one.assert_called_once_with({ '_id': _id })
             self.mock_collection.update.assert_called_once_with({ '_id': _id }, { '$set': article }, upsert = True)
