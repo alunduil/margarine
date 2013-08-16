b3e3c341c534fea38971dce9204a9bedc0e31846
Alex Brandt <alunduil@alunduil.com>
2013-08-16 11:43:02 -0500

    Add stub for integration test suite generation.

--- test_margarine/test_integration/__init__.py
+++ test_margarine/test_integration/__init__.py
@@ -3,3 +3,24 @@
 # margarine is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+def power_of_units():
+    '''Import unit tests and redfine to create base integration tests.
+
+    An excellent start to an integration test suite is using the unit tests
+    already defined.  Coupling this with a reduction in mocks following the
+    powerset of all mocks used should generate a comprehensive integration
+    suite as well as a basic functional suite (no mocks).  Using this idea this
+    function imports all unit tests, the set of all mocks used, and generates
+    the integration test suite.
+
+    .. note::
+        Integration tests can still be added by hand but probably should
+        correlate to a unit test in all cases.
+
+    '''
+
+    pass
