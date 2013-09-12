a7e98a6bb9ccb86d6f2628fefad383b05b5cd584
Alex Brandt <alunduil@alunduil.com>
2013-09-11 22:57:49 -0500

    Add os import to configuration parameters.

--- margarine/parameters/configuration.py
+++ margarine/parameters/configuration.py
@@ -5,11 +5,9 @@
 # margarine is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-import logging
+import os
 
 from margarine.parameters import Parameters
-
-logger = logging.getLogger(__name__)
 
 DIRECTORY = os.path.join(os.path.sep, 'etc', 'margarine')
 FILE = os.path.join(DIRECTORY, 'margarine.ini')
