8b96c13d551c75d5d93359e33636eca629e7088a
Alex Brandt <alunduil@alunduil.com>
2013-09-11 22:51:35 -0500

    Add import of Parameters to flask parameters.

--- margarine/parameters/flask.py
+++ margarine/parameters/flask.py
@@ -5,6 +5,8 @@
 # margarine is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+from margarine.parameters import Paraemters
+
 Parameters('flask', parameters = [
     { # --flask-host=HOST; HOST ← 127.0.0.1
         'options': [ '--host' ],
