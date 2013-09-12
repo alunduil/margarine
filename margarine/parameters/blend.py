4d366c27ec60c5384282fc21cdcf19822073df3b
Alex Brandt <alunduil@alunduil.com>
2013-09-11 22:58:29 -0500

    Add socket import to blend parameters.

--- margarine/parameters/blend.py
+++ margarine/parameters/blend.py
@@ -5,6 +5,8 @@
 # margarine is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+import socket
+
 from margarine.parameters import Parameters
 
 Parameters('blend', parameters = [
