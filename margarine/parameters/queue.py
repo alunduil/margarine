7aba3d51c737d8983c03cf92ea18021f4c29ef64
Alex Brandt <alunduil@alunduil.com>
2013-09-11 22:35:12 -0500

    Add import of Parameters to queue parameters.

--- margarine/parameters/queue.py
+++ margarine/parameters/queue.py
@@ -5,6 +5,8 @@
 # margarine is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+from margarine.parameters import Parameters
+
 Parameters('queue', parameters = [
     { # --queue-url=URL; URL ← amqp://guest:guest@localhost
         'options': [ '--url' ],
