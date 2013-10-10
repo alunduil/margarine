50eff959d3a5f2d9eb701551b5c0576bac931ea0
Alex Brandt <alunduil@alunduil.com>
2013-10-10 16:49:29 -0500

    Change to single quotes in setup.py.

--- setup.py
+++ setup.py
@@ -12,43 +12,43 @@ from margarine import information
 
 PARAMS = {}
 
-PARAMS["name"] = information.NAME
-PARAMS["version"] = information.VERSION
-PARAMS["description"] = information.DESCRIPTION
-PARAMS["long_description"] = information.LONG_DESCRIPTION
-PARAMS["author"] = information.AUTHOR
-PARAMS["author_email"] = information.AUTHOR_EMAIL
-PARAMS["url"] = information.URL
-PARAMS["license"] = information.LICENSE
+PARAMS['name'] = information.NAME
+PARAMS['version'] = information.VERSION
+PARAMS['description'] = information.DESCRIPTION
+PARAMS['long_description'] = information.LONG_DESCRIPTION
+PARAMS['author'] = information.AUTHOR
+PARAMS['author_email'] = information.AUTHOR_EMAIL
+PARAMS['url'] = information.URL
+PARAMS['license'] = information.LICENSE
 
-PARAMS["classifiers"] = [
-        "Development Status :: 2 - Pre-Alpha",
-        "Environment :: No Input/Output (Daemon)",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7", # only tested on 2.7
-        "Programming Language :: Python :: 2 :: Only", # pika is not 3.* ready
-        "Topic :: Education",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+PARAMS['classifiers'] = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: No Input/Output (Daemon)',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7', # only tested on 2.7
+        'Programming Language :: Python :: 2 :: Only', # pika is not 3.* ready
+        'Topic :: Education',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
         ]
 
-PARAMS["keywords"] = [
-        "readability",
-        "delicious",
-        "bookmarks",
-        "margins",
-        "notes",
+PARAMS['keywords'] = [
+        'readability',
+        'delicious',
+        'bookmarks',
+        'margins',
+        'notes',
         ]
 
-PARAMS["provides"] = [
-        "margarine",
+PARAMS['provides'] = [
+        'margarine',
         ]
 
 with open('requirements.txt', 'r') as req_fh:
@@ -59,10 +59,10 @@ with open('test_margarine/requirements.txt', 'r') as req_fh:
 
 PARAMS['test_suite'] = 'nose.collector'
 
-PARAMS["scripts"] = [
-        "bin/blend",
-        "bin/spread",
-        "bin/tinge",
+PARAMS['scripts'] = [
+        'bin/blend',
+        'bin/spread',
+        'bin/tinge',
         ]
 
 # TODO Change to entry_points?
@@ -74,14 +74,14 @@ PARAMS["scripts"] = [
 #            ],
 #        }
 
-PARAMS["packages"] = [
-        "margarine",
-        "margarine.blend",
-        "margarine.spread",
-        "margarine.tinge",
+PARAMS['packages'] = [
+        'margarine',
+        'margarine.blend',
+        'margarine.spread',
+        'margarine.tinge',
         ]
 
-PARAMS["package_data"] = {
+PARAMS['package_data'] = {
         '': [
             'templates/*.html',
             'static/js/*.js',
@@ -90,18 +90,18 @@ PARAMS["package_data"] = {
             ],
         }
 
-PARAMS["data_files"] = [
-        ("share/doc/{P[name]}-{P[version]}".format(P = PARAMS), [
-            "README.rst",
+PARAMS['data_files'] = [
+        ('share/doc/{P[name]}-{P[version]}'.format(P = PARAMS), [
+            'README.rst',
             ]),
-        ("share/doc/{P[name]}-{P[version]}/config".format(P = PARAMS), [
-            "conf/logging.ini",
-            "conf/margarine.ini",
-            "conf/pyrax.ini",
+        ('share/doc/{P[name]}-{P[version]}/config'.format(P = PARAMS), [
+            'conf/logging.ini',
+            'conf/margarine.ini',
+            'conf/pyrax.ini',
             ]),
-        ("libexec/{P[name]}".format(P = PARAMS), [
-            "conf/blend.wsgi",
-            "conf/tinge.wsgi",
+        ('libexec/{P[name]}'.format(P = PARAMS), [
+            'conf/blend.wsgi',
+            'conf/tinge.wsgi',
             ])
         ]
 
