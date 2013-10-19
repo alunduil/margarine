ecefa03500ab09e00730428cbb89c6c87ca6e348
Alex Brandt <alunduil@alunduil.com>
2013-10-19 15:47:43 -0500

    Update documentation for article loader.

--- margarine/tinge/static/js/article.js
+++ margarine/tinge/static/js/article.js
@@ -1,35 +1,55 @@
 /**
- * Update the DOM with API information about the current article.
+ * Inject Article information into active DOM.
+ *
+ * This function expects to be called from an article page and updates the
+ * contents of the following items:
+ *
+ * :``.article-title``:  The title of the article.
+ * :``.article-author``: The author of the article.
+ * :``.article-date``:   The publication date of the article.
+ * :``.article-text``:   The text contents of the article.
+ *
+ * Arguments
+ * ---------
+ *
+ * :``apiEndpoint``: The endpoint that we can retrive article information from.
+ *
+ * .. note::
+ *   The article information is expected to be JSON and have keys corresponding
+ *   to the above information fields.
  */
 function updateArticle(apiEndpoint) {
   $.get(apiEndpoint + "articles/" + getUrlVars()["id"],
       function(data) {
-        $('.article-text').append(data.body);
-
-        $('.article-title').append(data.title);
         document.title = "Margarine—" + data.title; 
+        $('.article-title').append(data.title);
 
         $('.article-author').append(data.author);
         $('.article-date').append(data.date);
+
+        $('.article-text').append(data.body);
 
         $('.article-tags').append(data.tags ? data.tags.join() : "");
       }, "json" )
 }
 
 /**
- * @brief Convert current page's URL query parameters to an associative array.
+ * Convert current page's URL query parameters to an associative array.
  *
- * @returns Associate array mapping query parameter names to values.
+ * Returns
+ * -------
  *
- * Shamelessly implemented from:
- * http://jquery-howto.blogspot.com/2009/09/get-url-parameters-values-with-jquery.html 
+ * Associate array mapping query parameter names to values.
  *
+ * ..note::
+ *   Shamelessly implemented from:
+ *   http://jquery-howto.blogspot.com/2009/09/get-url-parameters-values-with-jquery.html 
  */
 function getUrlVars() {
   var vars = [], hash;
   var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
 
-  for ( var i = 0; i < hashes.length; i++) {
+  for ( var i = 0; i < hashes.length; i++ ) {
     hash = hashes[i].split('=');
     vars.push(hash[0]);
     vars[hash[0]] = hash[1];
@@ -37,4 +57,3 @@ function getUrlVars() {
 
   return vars;
 }
-
