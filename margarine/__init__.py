e749597c7e7f7b544851a5f1b91c9285438f0164
Alex Brandt <alunduil@alunduil.com>
2014-03-08 16:36:47 -0600

    Add margarine wrapper.
    This wrapper parameterizes running margarine daemons so that we can
    create a general purpose Dockerfile.

--- margarine/__init__.py
+++ margarine/__init__.py
@@ -3,3 +3,23 @@
 # margarine is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+import margarine.blend
+import margarine.tinge
+import margarine.spread
+
+def run():
+    parser = argparse.ArgumentParser(description = 'Margarine Wrapper')
+
+    choices = ( 'blend', 'tinge', 'spread' )
+    parser.add_argument('applications',
+        nargs = '*',
+        default = choices,
+        choices = choices,
+        help = 'List of margarine daemons to run. Default: %(default)s',
+        metavar = 'APP'
+    )
+
+    arguments = parser.parse_args()
+
+    for application in arguments.applications:
+        getattr(margarine, application).run()
--- setup.py
+++ setup.py
@@ -53,18 +53,14 @@ PARAMS['classifiers'] = [
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: No Input/Output (Daemon)',
         'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
+        'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7', # only tested on 2.7
         'Programming Language :: Python :: 2 :: Only', # pika is not 3.* ready
-        'Topic :: Education',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
         ]
 
 PARAMS['keywords'] = [
@@ -89,18 +85,19 @@ PARAMS['test_suite'] = 'nose.collector'
 
 PARAMS['entry_points'] = {
         'console_scripts': [
-            'tinge = margarine.tinge:run',
             'blend = margarine.blend:run',
+            'margarine = margarine:run',
             'spread = margarine.spread:run',
+            'tinge = margarine.tinge:run',
             ],
         }
 
 PARAMS['packages'] = [
         'margarine',
         'margarine.blend',
+        'margarine.parameters',
         'margarine.spread',
         'margarine.tinge',
-        'margarine.parameters',
         ]
 
 PARAMS['package_data'] = {
@@ -119,12 +116,7 @@ PARAMS['data_files'] = [
         ('share/doc/{P[name]}-{P[version]}/config'.format(P = PARAMS), [
             'config/logging.ini',
             'config/margarine.ini',
-            'config/pyrax.ini',
             ]),
-        ('libexec/{P[name]}'.format(P = PARAMS), [
-            'scripts/blend.wsgi',
-            'scripts/tinge.wsgi',
-            ])
         ]
 
 setup(**PARAMS)
