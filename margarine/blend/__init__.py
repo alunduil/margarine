005b4b18c78c4f0a6661d962dc8fd23fbedf4822
Alex Brandt <alunduil@alunduil.com>
2014-03-08 20:35:02 -0600

    Add debug support to tornado starter.

--- margarine/blend/__init__.py
+++ margarine/blend/__init__.py
@@ -40,11 +40,18 @@ BLEND_APPLICATION = tornado.web.Application(
 
 def run():
     http_server = tornado.httpserver.HTTPServer(BLEND_APPLICATION)
-    http_server.bind(
-        port = PARAMETERS['tornado.port'],
-        address = PARAMETERS['tornado.address']
-    )
-    http_server.start(0)
+
+    if PARAMETERS['tornado.debug']:
+        http_server.listen(
+            port = PARAMETERS['tornado.port'],
+            address = PARAMETERS['tornado.address']
+        )
+    else:
+        http_server.bind(
+            port = PARAMETERS['tornado.port'],
+            address = PARAMETERS['tornado.address']
+        )
+        http_server.start(0)
 
     tornado.ioloop.IOLoop.instance().start()
 
